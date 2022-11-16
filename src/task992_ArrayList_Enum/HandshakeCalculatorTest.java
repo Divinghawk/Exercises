cf103b361fb8b9a2e53bb8a5693e162f943f3ef2
Divinghawk <Paulsadlowski@web.de>
2022-11-16 19:10:34 +0100

    Instructions

--- src/task992_ArrayList_Enum/HandshakeCalculatorTest.java
+++ src/task992_ArrayList_Enum/HandshakeCalculatorTest.java
@@ -14,11 +14,7 @@ class HandshakeCalculatorTest {
 	    Assert.assertEquals(Collections.EMPTY_LIST, handshakeCalculator.calculateHandshake(16));
 	}
 
-//	@Test
-//	public void testThatHandlesMoreThanFiveBinaryPlacesWithoutReversal() {
-//	    Assert.assertEquals(handshakeCalculator.calculateHandshake(35)).containsExactly(Signal.WINK, Signal.DOUBLE_BLINK);
-//	}
-	
+
 //	@Test
 //	public void testThatHandlesMoreThanFiveBinaryPlacesWithoutReversal() {
 //	    assertThat(handshakeCalculator.calculateHandshake(35)).containsExactly(Signal.WINK, Signal.DOUBLE_BLINK);
